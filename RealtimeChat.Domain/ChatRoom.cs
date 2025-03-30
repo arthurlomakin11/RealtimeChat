8dcc250079560a16cf376101aa21fffd9fefa6ba
arthurlomakin11 <arthurlomakin11@gmail.com>
2025-03-30 20:27:12 +0300

    Add = null!; in Domain

--- RealtimeChat.Domain/ChatRoom.cs
+++ RealtimeChat.Domain/ChatRoom.cs
@@ -3,8 +3,8 @@
 public class ChatRoom
 {
     public int Id { get; set; } 
-    public string Name { get; set; }
+    public string Name { get; set; } = null!;
     
-    public List<Message> Messages { get; set; }
-    public List<ChatRoomParticipant> Participants { get; set; }
+    public List<Message> Messages { get; set; } = null!;
+    public List<ChatRoomParticipant> Participants { get; set; } = null!;
 }
